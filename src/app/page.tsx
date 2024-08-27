229084f246fdf57a6a162f4045a299284cbf1aec
Lukas Koenig <Lkoenig2121@gmail.com>
2024-08-27 19:45:23 -0400

    Good fundamentals down

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">
+        Welcome to my blog
+      </h1>
+      <p className="max-w-[750px] mx-auto leading-10">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam optio at
+        possimus, tenetur iure sequi? Esse cum modi voluptate molestias,
+        accusamus aperiam doloribus porro dignissimos sapiente, corporis
+        excepturi inventore ipsa!
+      </p>
+    </main>
+  );
+}
