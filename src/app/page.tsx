08552886f497ab8d802a217a8d9d352fe80f0aa0
Lukas Koenig <Lkoenig2121@gmail.com>
2024-09-02 13:02:21 -0400

    Now Lukey's Blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="text-center pt-32 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">
-        Welcome to my blog
+        Welcome to Lukey&apos;s blog
       </h1>
       <p className="max-w-[750px] mx-auto leading-10">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam optio at
